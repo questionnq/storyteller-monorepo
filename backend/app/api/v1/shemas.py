bc41a95f313b7ef532ce5f7d0e0ba3f542c16394
questionnq <questionnq@gmail.com>
2025-11-10 23:04:06 +0300

    Update shemas.py

--- backend/app/api/v1/shemas.py
+++ backend/app/api/v1/shemas.py
@@ -1,7 +1,7 @@
 from pydantic import BaseModel, Field
 
 class ScriptRequest(BaseModel):
-    prompt: str | None = Field(max_lenght = 300)
-    genre: str | None = Field(max_lenght = 50)
-    style: str | None = Field(max_lenght = 50)
+    prompt: str | None = Field(max_length = 300)
+    genre: str | None = Field(max_length = 50)
+    style: str | None = Field(max_length = 50)
     time: float | None = Field(ge = 0, le = 150)
