3ae9976f80def7481bf93c88799ef279345cadce
Petunov Nikita <petunovnikita89@mail.ru>
2025-11-11 08:11:52 +0300

    fix middleware in main.py 2

--- backend/main.py
+++ backend/main.py
@@ -4,12 +4,14 @@ from app.api.v1.routes import router as api_router
 
 app = FastAPI(title="Script Generator")
 
+https://storyteller-monorepo.vercel.app
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:3000", "http://127.0.0.1:3000", "https://storyteller-monorepo.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"],
+    allow_headers=["*", "Authorization"],
 )
 
 app.include_router(api_router)
